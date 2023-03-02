efef29d6fc35dfe9b6065bd3579128e7dfb26ef2
madson <madsonsilva2005@gmail.com>
2023-03-02 10:48:24 -0300

    utilizada conexão através de variaveis de ambiente para melhor segurança

--- src/data/db.js
+++ src/data/db.js
@@ -1,16 +1,17 @@
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connect = async () => {
-  
   if (global.connection) {
     return global.connection.connect();
   }
 
   const pool = new pg.Pool({
-    connectionString:
-      "postgres://qyemqexq:5wl-H-rsRpqlE2YmigrODjlQ8EGqj8-K@mahmud.db.elephantsql.com/qyemqexq",
+    connectionString: process.env.DB_URL,
   });
-  
+
   global.connection = pool;
 
   return pool.connect();
